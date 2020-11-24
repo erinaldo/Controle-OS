61ad16dc28e3b15e9ab373ef7d3cf36a78123d0c
Mackson Roberto Santos <macksonmr@outlook.com>
2020-11-24 11:07:55 -0300

    adicionado a consulta de cnpj

---
+++ Formularios/Ferramentas/frmConsultaCNPJ.vb
@@ -0,0 +1,7 @@
+﻿Imports DllConsultaCNPJ
+
+Public Class frmConsultaCNPJ
+        Private Sub frmConsultaCNPJ_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        End Sub
+End Class
